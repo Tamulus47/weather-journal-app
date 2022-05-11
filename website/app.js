73841a9cad7bba96e0e0cf62a1589667a96c8340
Tamulus <mahmoud.mostafa0047@gmail.com>
2022-05-12 01:25:52 +0200

    add feelings to content

--- website/app.js
+++ website/app.js
@@ -3,8 +3,13 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
-let addDate = document.getElementById('date');
-let btn = document.getElementById('generate');
+const addDate = document.getElementById('date');
+const btn = document.getElementById('generate');
+const content = document.getElementById('content');
+let feel = document.getElementById('feelings');
+
+
 btn.addEventListener('click', () => {
-    addDate.innerHTML = `${newDate}`;
+    content.innerHTML = `you feeling: ${feel.value}`;
+    addDate.innerHTML = `date is: ${newDate}`;
 })
